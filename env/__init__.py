32362d5d99963aa29959040af049a2b1ecc1d992
Joywin Bennis <21a20.joywin@sjec.ac.in>
2026-04-12 15:55:33 +0530

    removed the v1 environemnt

--- env/__init__.py
+++
@@ -1,6 +0,0 @@
-"""CRISPR editing environment package."""
-
-from .environment import CrisprEnv
-from .models import Action, ActionType, EnvironmentState, GuideObservation, GuideRNA
-
-__all__ = ["CrisprEnv", "Action", "ActionType", "EnvironmentState", "GuideObservation", "GuideRNA"]
